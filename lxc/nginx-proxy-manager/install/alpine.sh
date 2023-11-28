eae6388e0f40b3fb7d75214c967612f2a85fe2ee
xiaohaiming <xiaohaiming@shoplineapp.com>
2023-11-28 14:24:27 +0800

    refactor alpine step1

--- lxc/nginx-proxy-manager/install/alpine.sh
+++ lxc/nginx-proxy-manager/install/alpine.sh
@@ -1,30 +1,6 @@
 #!/usr/bin/env sh
 
-set -eux
-set -o pipefail
-
-trap trapexit EXIT SIGTERM
-
-TEMPDIR="$1"
-[ -d "${TEMPDIR}" ] || TEMPDIR=$(mktemp -d)
-TEMPLOG="$TEMPDIR/tmplog"
-TEMPERR="$TEMPDIR/tmperr"
-LASTCMD=""
-WGETOPT="-t 2 -T 15 -q"
-DEVDEPS="npm g++ make gcc git python3-dev musl-dev libffi-dev openssl-dev jq"
-NPMURL="https://github.com/NginxProxyManager/nginx-proxy-manager"
-source <(cat /etc/os-release | tr -s '\n' | awk '{print "OS_"$0}')
-cd $TEMPDIR
-touch "$TEMPLOG"
-
-# Helpers
-log() {
-  logs=$(cat $TEMPLOG | sed -e "s/34/32/g" | sed -e "s/info/success/g");
-  #clear && printf "\033c\e[3J$logs\n\e[34m[info] $*\e[0m\n" | tee $TEMPLOG;
-  printf "\033c\e[3J$logs\n\e[34m[info] $*\e[0m\n" | tee $TEMPLOG;
-}
-
-trapexit() {
+trapexit_alpine() {
   status=$?
 
   if [[ $status -eq 0 ]]; then
@@ -42,160 +18,179 @@ trapexit() {
   apk del $DEVDEPS &>/dev/null
 }
 
-sed -i 's/dl-cdn.alpinelinux.org/mirrors.tuna.tsinghua.edu.cn/g' /etc/apk/repositories
-# Update container OS
-log "Updating container OS"
-echo "fs.file-max = 65535" > /etc/sysctl.conf
-apk update
-apk upgrade
-adduser npm --shell=/bin/false --no-create-home -D
-
 # Check for previous install
-if [ -f /etc/init.d/npm ]; then
-  log "Stopping services"
-  rc-service npm stop &>/dev/null
-  rc-service openresty stop &>/dev/null
-  sleep 2
+pre_install() {
+  log "Updating container OS"
+  echo "fs.file-max = 65535" > /etc/sysctl.conf
+  if [ -f /etc/init.d/npm ]; then
+    log "Stopping services"
+    rc-service npm stop &>/dev/null
+    rc-service openresty stop &>/dev/null
+    sleep 2
 
-  log "Cleaning old files"
-  # Cleanup for new install
-  rm -rf /app \
-  /var/www/html \
-  /etc/nginx \
-  /var/log/nginx \
-  /var/lib/nginx \
-  /var/cache/nginx &>/dev/null
+    log "Cleaning old files"
+    # Cleanup for new install
+    rm -rf /app \
+    /var/www/html \
+    /etc/nginx \
+    /var/log/nginx \
+    /var/lib/nginx \
+    /var/cache/nginx &>/dev/null
 
-  log "Removing old dependencies"
-  apk del certbot $DEVDEPS &>/dev/null
-fi
+    log "Removing old dependencies"
+    apk del certbot $DEVDEPS &>/dev/null
+  fi
+}
 
-OPENRESTY_REP_PREFIX="https://mirrors.ustc.edu.cn/openresty"
-log "Checking for latest openresty repository"
-ALPINE_MAJOR_VER=$(echo $OS_VERSION_ID | sed 's/\.[0-9]\+$//')
-# add openresty public key
-if [ ! -f /etc/apk/keys/admin@openresty.com-5ea678a6.rsa.pub ]; then
-  wget $WGETOPT -P /etc/apk/keys/ $OPENRESTY_REP_PREFIX/admin@openresty.com-5ea678a6.rsa.pub
-fi
-# Update/Insert openresty repository
-sed -i '/openresty/d' /etc/apk/repositories
-echo "$OPENRESTY_REP_PREFIX/alpine/v$ALPINE_MAJOR_VER/main" \
-    | tee -a /etc/apk/repositories
-apk update
+install_depend() {
+    log "Installing dependencies"
+    # Install dependancies
+    DEVDEPS="npm g++ make gcc git python3-dev musl-dev libffi-dev openssl-dev jq"
+    adduser npm --shell=/bin/false --no-create-home -D
+    apk upgrade
+    apk openssl apache2-utils logrotate $DEVDEPS
+}
 
-# Install dependancies
-log "Installing dependencies"
-apk add python3 py3-pip openresty nodejs yarn openssl apache2-utils logrotate $DEVDEPS
-#python3 -m ensurepip --upgrade
-pip3 config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple/
-pip3 config set install.trusted-host pypi.tuna.tsinghua.edu.cn
-pip3 config list
-pip3 install --upgrade pip
-# Setup python env and PIP
-log "Setting up python"
-python3 -m venv /opt/certbot/
-[ -f ~/.profile ] || touch ~/.profile && chmod 0644 ~/.profile
-echo "source /opt/certbot/bin/activate" >> ~/.profile
-source ~/.profile
+install_nodejs() {
+  apk add nodejs yarn
+}
 
-# Install certbot and python dependancies
-pip3 install --no-cache-dir -U cryptography==3.3.2
-pip3 install --no-cache-dir cffi certbot
+install_openresty() {
+  OPENRESTY_REP_PREFIX="https://mirrors.ustc.edu.cn/openresty"
+  log "Checking for latest openresty repository"
+  ALPINE_MAJOR_VER=$(echo $OS_VERSION_ID | sed 's/\.[0-9]\+$//')
+  # add openresty public key
+  if [ ! -f /etc/apk/keys/admin@openresty.com-5ea678a6.rsa.pub ]; then
+    wget $WGETOPT -P /etc/apk/keys/ $OPENRESTY_REP_PREFIX/admin@openresty.com-5ea678a6.rsa.pub
+  fi
+  # Update/Insert openresty repository
+  sed -i '/openresty/d' /etc/apk/repositories
+  echo "$OPENRESTY_REP_PREFIX/alpine/v$ALPINE_MAJOR_VER/main" \
+      | tee -a /etc/apk/repositories
+  apk update
+  apk add openresty
+}
 
-log "Checking for latest NPM release"
-# Get latest version information for nginx-proxy-manager
-URL_INFO_API_1="https://api.github.com/repos/NginxProxyManager/nginx-proxy-manager/releases/latest"
-URL_INFO_API_2="https://api.upup.cool/repo/NginxProxyManager/nginx-proxy-manager/info"
-PROXY_MANAGER_INFO="$(wget -qO - $URL_INFO_API_1 || wget -qO - URL_INFO_API_2)"
-_latest_version=$(echo $PROXY_MANAGER_INFO | jq -r 'if .version then .version else .tag_name end')
+install_python3() {
+  apk add python3 py3-pip
+  #python3 -m ensurepip --upgrade
+  pip3 config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple/
+  pip3 config set install.trusted-host pypi.tuna.tsinghua.edu.cn
+  pip3 config list
+  pip3 install --upgrade pip
+  # Setup python env and PIP
+  log "Setting up python"
+  python3 -m venv /opt/certbot/
+  [ -f ~/.profile ] || touch ~/.profile && chmod 0644 ~/.profile
+  echo "source /opt/certbot/bin/activate" >> ~/.profile
+  source ~/.profile
 
-# Download nginx-proxy-manager source
-PROXY_MANAGER_URL_1="https://mirror.ghproxy.com/${NPMURL}/archive/refs/tags/${_latest_version}.tar.gz"
-PROXY_MANAGER_URL_2="https://api.upup.cool/repo/NginxProxyManager/nginx-proxy-manager/source"
-PROXY_MANAGER_URL_3="${NPMURL}/archive/refs/tags/${_latest_version}.tar.gz"
-PROXY_DOWN_FILE="${_latest_version}.tar.gz"
-wget -O ${PROXY_DOWN_FILE} ${PROXY_MANAGER_URL_1} || wget -O ${PROXY_DOWN_FILE} ${PROXY_MANAGER_URL_2} || wget -O ${PROXY_DOWN_FILE} ${PROXY_MANAGER_URL_3}
-tar xzf ${PROXY_DOWN_FILE}
-cd nginx-proxy-manager-*
+  # Install certbot and python dependancies
+  pip3 install --no-cache-dir -U cryptography==3.3.2
+  pip3 install --no-cache-dir cffi certbot
+}
 
-log "Setting up enviroment"
-# Crate required symbolic links
-ln -sf /usr/bin/python3 /usr/bin/python
-ln -sf /usr/bin/pip3 /usr/bin/pip
-ln -sf /usr/bin/certbot /opt/certbot/bin/certbot
-ln -sf /usr/local/openresty/nginx/sbin/nginx /usr/sbin/nginx
-ln -sf /usr/local/openresty/nginx/ /etc/nginx
+download_NPM() {
+  log "Checking for latest NPM release"
+  # Get latest version information for nginx-proxy-manager
+  URL_INFO_API_1="https://api.github.com/repos/NginxProxyManager/nginx-proxy-manager/releases/latest"
+  URL_INFO_API_2="https://api.upup.cool/repo/NginxProxyManager/nginx-proxy-manager/info"
+  PROXY_MANAGER_INFO="$(wget -qO - $URL_INFO_API_1 || wget -qO - URL_INFO_API_2)"
+  _latest_version=$(echo $PROXY_MANAGER_INFO | jq -r 'if .version then .version else .tag_name end')
 
-# Update NPM version in package.json files
-sed -i "s+0.0.0+$_latest_version+g" backend/package.json
-sed -i "s+0.0.0+$_latest_version+g" frontend/package.json
+  # Download nginx-proxy-manager source
+  PROXY_MANAGER_URL_1="https://mirror.ghproxy.com/${NPMURL}/archive/refs/tags/${_latest_version}.tar.gz"
+  PROXY_MANAGER_URL_2="https://api.upup.cool/repo/NginxProxyManager/nginx-proxy-manager/source"
+  PROXY_MANAGER_URL_3="${NPMURL}/archive/refs/tags/${_latest_version}.tar.gz"
+  PROXY_DOWN_FILE="${_latest_version}.tar.gz"
+  wget -O ${PROXY_DOWN_FILE} ${PROXY_MANAGER_URL_1} || wget -O ${PROXY_DOWN_FILE} ${PROXY_MANAGER_URL_2} || wget -O ${PROXY_DOWN_FILE} ${PROXY_MANAGER_URL_3}
+  tar xzf ${PROXY_DOWN_FILE}
+  cd nginx-proxy-manager-*
+}
 
-# Fix nginx config files for use with openresty defaults
-sed -i 's+^daemon+#daemon+g' docker/rootfs/etc/nginx/nginx.conf
-NGINX_CONFS=$(find "$(pwd)" -type f -name "*.conf")
-for NGINX_CONF in $NGINX_CONFS; do
-  sed -i 's+include conf.d+include /etc/nginx/conf.d+g' "$NGINX_CONF"
-done
+set_up_NPM_env() {
+  log "Setting up enviroment"
+  # Crate required symbolic links
+  ln -sf /usr/bin/python3 /usr/bin/python
+  ln -sf /usr/bin/pip3 /usr/bin/pip
+  ln -sf /usr/bin/certbot /opt/certbot/bin/certbot
+  ln -sf /usr/local/openresty/nginx/sbin/nginx /usr/sbin/nginx
+  ln -sf /usr/local/openresty/nginx/ /etc/nginx
 
-# Copy runtime files
-mkdir -p /var/www/html /etc/nginx/logs
-cp -r docker/rootfs/var/www/html/* /var/www/html/
-cp -r docker/rootfs/etc/nginx/* /etc/nginx/
-cp docker/rootfs/etc/letsencrypt.ini /etc/letsencrypt.ini
-cp docker/rootfs/etc/logrotate.d/nginx-proxy-manager /etc/logrotate.d/nginx-proxy-manager
-ln -sf /etc/nginx/nginx.conf /etc/nginx/conf/nginx.conf
-rm -f /etc/nginx/conf.d/dev.conf
+  # Update NPM version in package.json files
+  sed -i "s+0.0.0+$_latest_version+g" backend/package.json
+  sed -i "s+0.0.0+$_latest_version+g" frontend/package.json
 
-# Create required folders
-mkdir -p /tmp/nginx/body \
-/run/nginx \
-/data/nginx \
-/data/custom_ssl \
-/data/logs \
-/data/access \
-/data/nginx/default_host \
-/data/nginx/default_www \
-/data/nginx/proxy_host \
-/data/nginx/redirection_host \
-/data/nginx/stream \
-/data/nginx/dead_host \
-/data/nginx/temp \
-/var/lib/nginx/cache/public \
-/var/lib/nginx/cache/private \
-/var/cache/nginx/proxy_temp
+  # Fix nginx config files for use with openresty defaults
+  sed -i 's+^daemon+#daemon+g' docker/rootfs/etc/nginx/nginx.conf
+  NGINX_CONFS=$(find "$(pwd)" -type f -name "*.conf")
+  for NGINX_CONF in $NGINX_CONFS; do
+    sed -i 's+include conf.d+include /etc/nginx/conf.d+g' "$NGINX_CONF"
+  done
 
-chmod -R 777 /var/cache/nginx
-chown root /tmp/nginx
+  # Copy runtime files
+  mkdir -p /var/www/html /etc/nginx/logs
+  cp -r docker/rootfs/var/www/html/* /var/www/html/
+  cp -r docker/rootfs/etc/nginx/* /etc/nginx/
+  cp docker/rootfs/etc/letsencrypt.ini /etc/letsencrypt.ini
+  cp docker/rootfs/etc/logrotate.d/nginx-proxy-manager /etc/logrotate.d/nginx-proxy-manager
+  ln -sf /etc/nginx/nginx.conf /etc/nginx/conf/nginx.conf
+  rm -f /etc/nginx/conf.d/dev.conf
 
-# Dynamically generate resolvers file, if resolver is IPv6, enclose in `[]`
-# thanks @tfmm
-echo resolver "$(awk 'BEGIN{ORS=" "} $1=="nameserver" {print ($2 ~ ":")? "["$2"]": $2}' /etc/resolv.conf);" > /etc/nginx/conf.d/include/resolvers.conf
+  # Create required folders
+  mkdir -p /tmp/nginx/body \
+  /run/nginx \
+  /data/nginx \
+  /data/custom_ssl \
+  /data/logs \
+  /data/access \
+  /data/nginx/default_host \
+  /data/nginx/default_www \
+  /data/nginx/proxy_host \
+  /data/nginx/redirection_host \
+  /data/nginx/stream \
+  /data/nginx/dead_host \
+  /data/nginx/temp \
+  /var/lib/nginx/cache/public \
+  /var/lib/nginx/cache/private \
+  /var/cache/nginx/proxy_temp
 
-# Generate dummy self-signed certificate.
-if [ ! -f /data/nginx/dummycert.pem ] || [ ! -f /data/nginx/dummykey.pem ]; then
-  log "Generating dummy SSL certificate"
-  openssl req -new -newkey rsa:2048 -days 3650 -nodes -x509 -subj "/O=Nginx Proxy Manager/OU=Dummy Certificate/CN=localhost" -keyout /data/nginx/dummykey.pem -out /data/nginx/dummycert.pem
-fi
+  chmod -R 777 /var/cache/nginx
+  chown root /tmp/nginx
 
-# Copy app files
-mkdir -p /app/global /app/frontend/images
-cp -r backend/* /app
-cp -r global/* /app/global
+  # Dynamically generate resolvers file, if resolver is IPv6, enclose in `[]`
+  # thanks @tfmm
+  echo resolver "$(awk 'BEGIN{ORS=" "} $1=="nameserver" {print ($2 ~ ":")? "["$2"]": $2}' /etc/resolv.conf);" > /etc/nginx/conf.d/include/resolvers.conf
 
-# Build the frontend
-log "Building frontend"
-cd ./frontend
-export NODE_ENV=development
-yarn install
-yarn build
-cp -r dist/* /app/frontend
-cp -r app-images/* /app/frontend/images
+  # Generate dummy self-signed certificate.
+  if [ ! -f /data/nginx/dummycert.pem ] || [ ! -f /data/nginx/dummykey.pem ]; then
+    log "Generating dummy SSL certificate"
+    openssl req -new -newkey rsa:2048 -days 3650 -nodes -x509 -subj "/O=Nginx Proxy Manager/OU=Dummy Certificate/CN=localhost" -keyout /data/nginx/dummykey.pem -out /data/nginx/dummycert.pem
+  fi
 
-# Initialize backend
-log "Initializing backend"
-rm -rf /app/config/default.json &>/dev/null
-if [ ! -f /app/config/production.json ]; then
-cat << 'EOF' > /app/config/production.json
+  # Copy app files
+  mkdir -p /app/global /app/frontend/images
+  cp -r backend/* /app
+  cp -r global/* /app/global
+}
+
+build_NPM_frontend() {
+  # Build the frontend
+  log "Building frontend"
+  cd ./frontend
+  export NODE_ENV=development
+  yarn install
+  yarn build
+  cp -r dist/* /app/frontend
+  cp -r app-images/* /app/frontend/images
+}
+
+init_NPM_backend() {
+  # Initialize backend
+  log "Initializing backend"
+  rm -rf /app/config/default.json &>/dev/null
+  if [ ! -f /app/config/production.json ]; then
+  cat << 'EOF' > /app/config/production.json
 {
   "database": {
     "engine": "knex-native",
@@ -208,14 +203,16 @@ cat << 'EOF' > /app/config/production.json
   }
 }
 EOF
-fi
-cd /app
-export NODE_ENV=development
-yarn install
+  fi
+  cd /app
+  export NODE_ENV=development
+  yarn install
+}
 
-# Create NPM service
-log "Creating NPM service"
-cat << 'EOF' > /etc/init.d/npm
+create_NPM_service() {
+  # Create NPM service
+  log "Creating NPM service"
+  cat << 'EOF' > /etc/init.d/npm
 #!/sbin/openrc-run
 description="Nginx Proxy Manager"
 
@@ -249,20 +246,22 @@ restart() {
   $0 start
 }
 EOF
-chmod a+x /etc/init.d/npm
-rc-update add npm boot &>/dev/null
-rc-update add openresty boot &>/dev/null
-rc-service openresty stop &>/dev/null
+  chmod a+x /etc/init.d/npm
+  rc-update add npm boot &>/dev/null
+  rc-update add openresty boot &>/dev/null
+  rc-service openresty stop &>/dev/null
+}
 
-# Start services
-log "Starting services"
-rc-service openresty start
-rc-service npm start
+start_now() {
+  # Start services
+  log "Starting services"
+  rc-service openresty start
+  rc-service npm start
+  IP=$(ip a s dev eth0 | sed -n '/inet / s/\// /p' | awk '{print $2}')
+  log "Installation complete
 
-IP=$(ip a s dev eth0 | sed -n '/inet / s/\// /p' | awk '{print $2}')
-log "Installation complete
+  \e[0mNginx Proxy Manager should be reachable at the following URL.
 
-\e[0mNginx Proxy Manager should be reachable at the following URL.
-
-      http://${IP}:81
-"
+        http://${IP}:81
+  "
+}
--- lxc/nginx-proxy-manager/setup.sh
+++ lxc/nginx-proxy-manager/setup.sh
@@ -1,24 +1,54 @@
 #!/usr/bin/env sh
 
+set -eux
+set -o pipefail
+
+SUPPORTED_OS="debian alpine"
+
+TEMPDIR=$(mktemp -d)
+TEMPLOG="$TEMPDIR/tmplog"
+TEMPERR="$TEMPDIR/tmperr"
+NPMURL="https://github.com/NginxProxyManager/nginx-proxy-manager"
+cd $TEMPDIR && mkdir -p install && touch "$TEMPLOG"
+LASTCMD=""
+
+
 exit_with_msg() {
   echo "ERROR: $2" 1>&2
   exit $1
 }
 
-TEMPDIR=$(mktemp -d)
-cd $TEMPDIR && mkdir -p install
+log() {
+  logs=$(cat $TEMPLOG | sed -e "s/34/32/g" | sed -e "s/info/success/g");
+  #clear && printf "\033c\e[3J$logs\n\e[34m[info] $*\e[0m\n" | tee $TEMPLOG;
+  printf "\033c\e[3J$logs\n\e[34m[info] $*\e[0m\n" | tee $TEMPLOG;
+}
+
+prepare_dep_debian() {
+  echo todo...
+}
+prepare_dep_alpine() {
+  sed -i 's/dl-cdn.alpinelinux.org/mirrors.tuna.tsinghua.edu.cn/g' /etc/apk/repositories
+  WGETOPT="-t 2 -T 15 -q"
+  apk update
+  apk add wget
+}
 
 [ -f /etc/os-release ] || exit_with_msg 100 "OS Not Supported"
 source <(cat /etc/os-release | tr -s '\n' | sed 's/ubuntu/debian/' | awk '{print "OS_"$0}')
 #CURRENT_SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
-[ -z "${OS_ID}" ] && exit_with_msg 101 "OS Not Supported"
+for the_os in $SUPPORTED_OS; do
+  [ "$the_os" = "$OS_ID" ] && IS_SUPPORTED=true && break
+done
+[ ! $IS_SUPPORTED ] && exit_with_msg 101 "OS Not Supported"
 [ -n "$(grep 'kthreadd' /proc/2/status 2>/dev/null)" ] && exit_with_msg 102 "Only Supported In Container"
+
+prepare_dep_${OS_ID}
 
 if [ -f "$1" ]; then
   INSTALL_SCRIPT="$1"
 else
   NSTALL_SCRIPT="$TEMPDIR/install/${OS_ID}.sh"
-  mkdir -p "$TEMPDIR/install"
   wget -O ${INSTALL_SCRIPT} https://fastly.jsdelivr.net/gh/hyamine/proxmox-scripts@main/lxc/nginx-proxy-manager/install/$OS_ID.sh
 fi
 [ ! -f "$NSTALL_SCRIPT" ] && exit_with_msg 103 "OS Not Supported or Download install file failed"
@@ -39,8 +69,24 @@ echo "TZ=Asia/Shanghai" >> /etc/environment
 #echo 'registry "https://registry.npmmirror.com"' > ~/.yarnrc
 #chmod 0644 ~/.yarnrc /usr/local/share/.yarnrc ~/.config/pip/pip.conf
 
-if [ "$(command -v bash)" ]; then
-  $(command -v sudo) bash "$INSTALL_SCRIPT" "$TEMPDIR"
-else
-  sh "$INSTALL_SCRIPT" "$TEMPDIR"
-fi
+#if [ "$(command -v bash)" ]; then
+#  $(command -v sudo) bash "$INSTALL_SCRIPT" "$TEMPDIR"
+#else
+#  sh "$INSTALL_SCRIPT" "$TEMPDIR"
+#fi
+
+. "$NSTALL_SCRIPT"
+
+trap trapexit_${OS_ID} EXIT SIGTERM
+
+pre_install
+install_depend
+install_nodejs
+install_openresty
+install_python3
+download_NPM
+set_up_NPM_env
+build_NPM_frontend
+init_NPM_backend
+create_NPM_service
+start_now
